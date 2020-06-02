c73ed56ac9cb573cff4b5958cd843b570a737ddf
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-06-02 20:26:55 +0600

    warmUpExercise complete

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -18,7 +18,9 @@ figure; % open a new figure window
 
 
 
-
+plot(x, y, 'rx', 'MarkerSize', 10);
+ylabel('Profit in $10,000s');
+xlabel('Population of city in 10,000s');
 
 % ============================================================
 
