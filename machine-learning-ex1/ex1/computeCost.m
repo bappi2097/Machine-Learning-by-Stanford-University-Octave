6f20dbddc7324891037338d2447b7bf176000480
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-06-02 21:37:30 +0600

    computeCost complete

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -7,7 +7,7 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+J = (1/(2*m))*(sum(((X*theta)-y).^2));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
