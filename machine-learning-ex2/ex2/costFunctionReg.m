2dde2918534cefbda2e37b13b92f68d7419ed1c7
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-06-15 14:38:16 +0600

    Costunction with regularization

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -26,11 +26,6 @@ J = ((-y'*log(h)-((1-y)'*log(1-h)))/m)+regu;
 
 grad = ((X'*(h-y))/m)+((lambda*theta)/m);
 
-% h_theta = sigmoid(X*theta); % h_theta is a size(X,1) by 1 vector
-
-% J = (-y' * log(h_theta) - (1-y)' * log(1-h_theta))/m + regu;%lambda/2/m*theta(2:end)'*theta(2:end);
-
-% grad = X'*(h_theta - y)/m + lambda*theta/m;
 
 % grad(1) -= lambda*theta(1)/m;
 % =============================================================
